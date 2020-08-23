368cac2d340728a16275fb4094e727b5522b89f9
Duy Lam Nguyen <49040840+duylamnguyen@users.noreply.github.com>
2020-08-23 11:10:59 +0700

    Update function.R

--- function.R
+++ function.R
@@ -7,3 +7,30 @@ sum_even.function <- function(start, end){
   }
   print(sum_even)
 }
+
+tinh_tien_dien_v201903.function <- function(so_kw){
+    muc1 = 1678
+    muc2 = 1734
+    muc3 = 2014
+    muc4 = 2536
+    muc5 = 2834
+    muc6 = 2927
+
+    bac50 = 50
+    bac100 = 100
+
+    if(so_kw <= 50){
+        tien_dien = so_kw * muc1
+    } else if(so_kw <= 100){
+        tien_dien = bac50 * muc1 + (so_kw - bac50) * muc2
+    } else if(so_kw <= 200){
+        tien_dien = bac50 * muc1 + bac50 * muc2 + (so_kw - bac100) * muc3
+    } else if(so_kw <= 300){
+        tien_dien = bac50 * muc1 + bac50 * muc2 + bac100 * muc3 + (so_kw - bac50 - bac50 - bac100) * muc4
+    } else if(so_kw <= 400){
+        tien_dien = bac50 * muc1 + bac50 * muc2 + bac100 * muc3 + bac100 * muc4 + (so_km - bac50 - bac50 - bac100 - bac100) * muc5
+    } else{
+        tien_dien = bac50 * muc1 + bac50 * muc2 + bac100 * muc3 + bac100 * muc4 + bac100 * muc5 + (so_kw - bac50 - bac50 - bac100 - bac100 - bac100) * muc6
+    }
+    print(paste('Tien dien:', tien_dien, 'VND'))
+}
